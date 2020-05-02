030b1f14b1ec0a0390dbdd02f7f0bda7b33767e0
OhhGee <skylion.scp@gmail.com>
2020-05-02 19:10:24 +0200

    Multithread

--- BS06/cmake-build-debug/cmake_install.cmake
+++ BS06/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ohgee/Desktop/BS06
+# Install script for directory: /home/ohgee/Documents/Betriebssysteme/BS06
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ohgee/Desktop/BS06/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ohgee/Documents/Betriebssysteme/BS06/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
